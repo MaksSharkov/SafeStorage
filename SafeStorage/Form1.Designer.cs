49e308537960086f423230fc236488279bffd105
MaksSharkov <makssharkov@yandex.ru>
2016-04-12 02:36:08 +0300

    I'ts not working! =( Somethimg wrong...

---
+++ SafeStorage/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace SafeStorage
+{
+    partial class SafeStorage
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SafeStorage));
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStorage = new System.Windows.Forms.ToolStripMenuItem();
+            this.openStorage = new System.Windows.Forms.ToolStripMenuItem();
+            this.createStorageFile = new System.Windows.Forms.SaveFileDialog();
+            this.addFileInStorage = new System.Windows.Forms.OpenFileDialog();
+            this.openStorageFile = new System.Windows.Forms.OpenFileDialog();
+            this.labelStorage = new System.Windows.Forms.Label();
+            this.labelStoragePath = new System.Windows.Forms.Label();
+            this.contentStorage = new System.Windows.Forms.ListBox();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.progressAdd = new System.Windows.Forms.ProgressBar();
+            this.ExtractFile = new System.Windows.Forms.Button();
+            this.extractFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.buttonCrypt = new System.Windows.Forms.Button();
+            this.decryptDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(594, 24);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newStorage,
+            this.openStorage});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(53, 20);
+            this.Menu.Text = "Меню";
+            // 
+            // newStorage
+            // 
+            this.newStorage.Name = "newStorage";
+            this.newStorage.Size = new System.Drawing.Size(189, 22);
+            this.newStorage.Text = "Создать хранилище";
+            this.newStorage.Click += new System.EventHandler(this.создатьХранилищеToolStripMenuItem_Click);
+            // 
+            // openStorage
+            // 
+            this.openStorage.Name = "openStorage";
+            this.openStorage.Size = new System.Drawing.Size(189, 22);
+            this.openStorage.Text = "Открыть хранилище";
+            this.openStorage.Click += new System.EventHandler(this.openStorage_Click);
+            // 
+            // createStorageFile
+            // 
+            this.createStorageFile.DefaultExt = "msh";
+            this.createStorageFile.Filter = "Storage files|*.stor";
+            // 
+            // addFileInStorage
+            // 
+            this.addFileInStorage.Filter = "Все файлы|*.*";
+            // 
+            // openStorageFile
+            // 
+            this.openStorageFile.Filter = "Storage files|*.stor";
+            // 
+            // labelStorage
+            // 
+            this.labelStorage.AutoSize = true;
+            this.labelStorage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStorage.Location = new System.Drawing.Point(12, 35);
+            this.labelStorage.Name = "labelStorage";
+            this.labelStorage.Size = new System.Drawing.Size(165, 17);
+            this.labelStorage.TabIndex = 1;
+            this.labelStorage.Text = "Текущее хранилище:";
+            // 
+            // labelStoragePath
+            // 
+            this.labelStoragePath.AutoSize = true;
+            this.labelStoragePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStoragePath.Location = new System.Drawing.Point(173, 39);
+            this.labelStoragePath.Name = "labelStoragePath";
+            this.labelStoragePath.Size = new System.Drawing.Size(24, 13);
+            this.labelStoragePath.TabIndex = 2;
+            this.labelStoragePath.Text = "N/A";
+            // 
+            // contentStorage
+            // 
+            this.contentStorage.FormattingEnabled = true;
+            this.contentStorage.Location = new System.Drawing.Point(15, 55);
+            this.contentStorage.Name = "contentStorage";
+            this.contentStorage.Size = new System.Drawing.Size(447, 290);
+            this.contentStorage.TabIndex = 3;
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Location = new System.Drawing.Point(468, 55);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(114, 23);
+            this.buttonAddFile.TabIndex = 4;
+            this.buttonAddFile.Text = "Добавить файл";
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // progressAdd
+            // 
+            this.progressAdd.Location = new System.Drawing.Point(15, 351);
+            this.progressAdd.Name = "progressAdd";
+            this.progressAdd.Size = new System.Drawing.Size(447, 23);
+            this.progressAdd.Step = 1;
+            this.progressAdd.TabIndex = 5;
+            // 
+            // ExtractFile
+            // 
+            this.ExtractFile.Location = new System.Drawing.Point(468, 84);
+            this.ExtractFile.Name = "ExtractFile";
+            this.ExtractFile.Size = new System.Drawing.Size(114, 23);
+            this.ExtractFile.TabIndex = 6;
+            this.ExtractFile.Text = "Извлечь файл";
+            this.ExtractFile.UseVisualStyleBackColor = true;
+            this.ExtractFile.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(468, 267);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(114, 20);
+            this.passwordBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(468, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Пароль:";
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(468, 322);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(114, 23);
+            this.buttonDecrypt.TabIndex = 9;
+            this.buttonDecrypt.Text = "Расшифровать";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // buttonCrypt
+            // 
+            this.buttonCrypt.Location = new System.Drawing.Point(468, 293);
+            this.buttonCrypt.Name = "buttonCrypt";
+            this.buttonCrypt.Size = new System.Drawing.Size(114, 23);
+            this.buttonCrypt.TabIndex = 10;
+            this.buttonCrypt.Text = "Зашифровать";
+            this.buttonCrypt.UseVisualStyleBackColor = true;
+            this.buttonCrypt.Click += new System.EventHandler(this.buttonCrypt_Click);
+            // 
+            // decryptDialog
+            // 
+            this.decryptDialog.Filter = "Crypt storages|*.stcr";
+            // 
+            // SafeStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 375);
+            this.Controls.Add(this.buttonCrypt);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.ExtractFile);
+            this.Controls.Add(this.progressAdd);
+            this.Controls.Add(this.buttonAddFile);
+            this.Controls.Add(this.contentStorage);
+            this.Controls.Add(this.labelStoragePath);
+            this.Controls.Add(this.labelStorage);
+            this.Controls.Add(this.MainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MainMenu;
+            this.MaximizeBox = false;
+            this.Name = "SafeStorage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SafeStorage";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem newStorage;
+        private System.Windows.Forms.SaveFileDialog createStorageFile;
+        private System.Windows.Forms.OpenFileDialog addFileInStorage;
+        private System.Windows.Forms.ToolStripMenuItem openStorage;
+        private System.Windows.Forms.OpenFileDialog openStorageFile;
+        private System.Windows.Forms.Label labelStorage;
+        private System.Windows.Forms.Label labelStoragePath;
+        private System.Windows.Forms.ListBox contentStorage;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.ProgressBar progressAdd;
+        private System.Windows.Forms.Button ExtractFile;
+        private System.Windows.Forms.SaveFileDialog extractFileDialog;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button buttonCrypt;
+        private System.Windows.Forms.OpenFileDialog decryptDialog;
+    }
+}
+
---
+++ SafeStorage/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SafeStorage
+{
+    public partial class SafeStorage : Form
+    {
+        string storagePath;
+        string addFile;
+        List<long> positionsFiles = new List<long> { };
+        List<long> lengthFiles = new List<long> { };
+        byte[] password16 = new byte[16];
+        byte[] test = new byte[5] { 1, 2, 3, 4, 5 };
+        static byte[] IV = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        static byte[] checkSumm = { 0x64, 0xFF, 0xA6, 0x25, 0x92, 0xB1, 0x0F, 0xAA, 0x26, 0x3F, 0x7B, 0xBB, 0x26, 0x79, 0x11, 0xC5 };
+
+        public SafeStorage()
+        {
+            InitializeComponent();
+        }
+
+        private void startAddFileInStorage(string file, string storage)
+        {
+            BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open));
+            BinaryWriter writer = new BinaryWriter(File.Open(storage, FileMode.Append));
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string fileName = fileInfo.Name;
+                long fileSize = fileInfo.Length;
+                //MessageBox.Show("Имя файла: " + fileName);
+                //MessageBox.Show("Размер файла(байт): " + fileSize);
+                long currentFile = writer.Seek(0, SeekOrigin.Current);
+                //MessageBox.Show("Текущий файл находится на " + currentFile + " байте");
+                writer.Write(fileName);
+                long nextFile = writer.Seek(0, SeekOrigin.Current) + sizeof(long) * 2 + fileSize;
+                //MessageBox.Show("Следующий файл будет на " + nextFile + " байте");
+                writer.Write(nextFile);
+                writer.Write(fileSize);
+                progressAdd.Maximum = (int)fileSize;
+                for (long i = 0; i < fileSize; i++)
+                {
+                    writer.Write(reader.ReadByte());
+                    progressAdd.Value++;
+                }
+                progressAdd.Value = 0;
+                reader.Close();
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                reader.Close();
+                writer.Close();
+            }
+
+        }
+
+        private void createFilesList(string storage)
+        {
+
+            BinaryReader reader = new BinaryReader(File.Open(storage, FileMode.Open));
+
+            try
+            {
+                contentStorage.Items.Clear();
+                string fileName;
+                long fileSize;
+                long currentFile = 0;
+                long previousFileLength = 0;
+                long nextFile;
+                byte[] trash = new byte[1];
+                byte stringLength;
+                while (reader.PeekChar() > -1)
+                {
+                    stringLength = (byte)reader.PeekChar();
+                    fileName = reader.ReadString();
+                    nextFile = reader.ReadInt64();
+                    fileSize = reader.ReadInt64();
+                    currentFile = stringLength + sizeof(long) * 2 + previousFileLength + 1;
+                    previousFileLength = fileSize + currentFile;
+                    contentStorage.Items.Add(fileName + getReadableFileSize(fileSize));
+                    positionsFiles.Add(currentFile);
+                    lengthFiles.Add(fileSize);
+                    for (long i = 0; i < fileSize; i++)
+                    {
+                        reader.Read(trash, 0, 1);
+                    }
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Файл поврежден и не может быть открыт!");
+                reader.Close();
+            }
+
+        }
+
+        private void extractFile(string storage, string name)
+        {
+
+            BinaryWriter writer = new BinaryWriter(File.Open(name, FileMode.Create));
+            BinaryReader reader = new BinaryReader(File.Open(storage, FileMode.Open));
+
+            try
+            {
+                int indexFile = contentStorage.SelectedIndex;
+                byte[] trash = new byte[1];
+                progressAdd.Maximum = (int)lengthFiles[indexFile];
+                for (long i = 0; i < positionsFiles[indexFile]; i++)
+                {
+                    reader.Read(trash, 0, 1);
+                }
+                for (long i = 0; i < lengthFiles[indexFile]; i++)
+                {
+                    writer.Write(reader.ReadByte());
+                    progressAdd.Value++;
+                }
+                progressAdd.Value = 0;
+                reader.Close();
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                reader.Close();
+                writer.Close();
+            }
+        }
+
+        private void создатьХранилищеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (createStorageFile.ShowDialog() == DialogResult.OK)
+            {
+                storagePath = createStorageFile.FileName;
+                if (!File.Exists(storagePath))
+                {
+                    BinaryWriter writer = new BinaryWriter(File.Open(storagePath, FileMode.Create));
+                    writer.Close();
+                    labelStoragePath.Text = storagePath;
+                    contentStorage.Items.Clear();
+                }
+                else MessageBox.Show("Такое хранилище уже существует!");
+            }
+        }
+
+        private void openStorage_Click(object sender, EventArgs e)
+        {
+            if (openStorageFile.ShowDialog() == DialogResult.OK)
+            {
+                storagePath = openStorageFile.FileName;
+                labelStoragePath.Text = storagePath;
+                createFilesList(storagePath);
+            }
+        }
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            if (storagePath == null)
+            {
+                MessageBox.Show("Сначала выберите хранилище!");
+            }
+            else
+            {
+                if (addFileInStorage.ShowDialog() == DialogResult.OK)
+                {
+                    addFile = addFileInStorage.FileName;
+                    startAddFileInStorage(addFile, storagePath);
+                    createFilesList(storagePath);
+                }
+            }
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            if (storagePath == null)
+            {
+                MessageBox.Show("Сначала выберите хранилище!");
+            }
+            else if (contentStorage.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите файл для извлечения!");
+            }
+            else
+            {
+                extractFileDialog.FileName = (string)contentStorage.SelectedItem;
+                if (extractFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    extractFile(storagePath, extractFileDialog.FileName);
+                }
+            }
+        }
+
+        private void getPassword()
+        {
+            using (MD5 md5hash = MD5.Create())
+            {
+                string fullPassword = GetMd5Hash(md5hash, passwordBox.Text);
+                for (int i = 0; i < 16; i++)
+                {
+                    password16[i] = (byte)fullPassword[i + 1];
+                }
+            }
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        private static byte[] Encrypt(byte[] ENC, byte[] AES_KEY, byte[] AES_IV)
+        {
+            using (Rijndael AES = Rijndael.Create())
+            {
+                //AES.KeySize = 128;
+                //AES.BlockSize = 128;
+                AES.IV = AES_IV;
+                AES.Key = AES_KEY;
+                AES.Padding = PaddingMode.None;
+
+                MemoryStream MS = new MemoryStream();
+                CryptoStream CS = new CryptoStream(MS, AES.CreateEncryptor(), CryptoStreamMode.Write);
+                CS.Write(ENC, 0, ENC.Length);
+                CS.Clear();
+                CS.Close();
+
+                return MS.ToArray();
+            }
+        }
+        private static byte[] Decrypt(byte[] DEC, byte[] AES_KEY, byte[] AES_IV)
+        {
+            using (Rijndael AES = Rijndael.Create())
+            {
+                //AES.KeySize = 128;
+                //AES.BlockSize = 128;
+                AES.IV = AES_IV;
+                AES.Key = AES_KEY;
+                AES.Padding = PaddingMode.None;
+
+                MemoryStream MS = new MemoryStream();
+                CryptoStream CS = new CryptoStream(MS, AES.CreateDecryptor(), CryptoStreamMode.Write);
+
+                CS.Write(DEC, 0, DEC.Length);
+                CS.Clear();
+                CS.Close();
+
+                return MS.ToArray();
+            }
+        }
+
+        private void buttonCrypt_Click(object sender, EventArgs e)
+        {
+            if (storagePath == null)
+            {
+                MessageBox.Show("Сначала выберите хранилище!");
+            }
+            else
+            {
+
+                FileInfo fileInfo = new FileInfo(storagePath);
+                string path = fileInfo.DirectoryName;
+                string name = fileInfo.Name;
+                name = name.Replace(fileInfo.Extension, "");
+                FileStream reader = File.Open(storagePath, FileMode.Open);
+                FileStream writer = File.Open(path + "\\" + name + ".stcr", FileMode.Create);
+
+                try
+                {
+                    getPassword();
+                    byte[] temp = new byte[16];
+                    int whole = (int)(reader.Length) / 16;
+                    int residue = (int)(reader.Length) % 16;
+                    progressAdd.Maximum = whole;
+                    for (int i = 0; i < whole; i++)
+                    {
+                        reader.Read(temp, 0, 16);
+                        temp = Encrypt(temp, password16, IV);
+                        writer.Write(temp, 0, 16);
+                        progressAdd.Value++;
+                    }
+                    if (residue != 0)
+                    {
+                        reader.Read(temp, 0, residue);
+                        temp = Encrypt(temp, password16, IV);
+                        writer.Write(temp, 0, residue);
+                    }
+                    progressAdd.Value = 0;
+                    reader.Close();
+                    writer.Close();
+                    File.Delete(storagePath);
+                    MessageBox.Show("Шифрование завершено!");
+                    storagePath = null;
+                    contentStorage.Items.Clear();
+                    labelStoragePath.Text = "N/A";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    reader.Close();
+                    writer.Close();
+                }
+            }
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            if (decryptDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                string cryptFilename = decryptDialog.FileName;
+                FileInfo fileInfo = new FileInfo(cryptFilename);
+                string path = fileInfo.DirectoryName;
+                string name = fileInfo.Name;
+                name = name.Replace(fileInfo.Extension, "");
+                string storageName = path + "\\" + name + ".stor";
+                FileStream reader = File.Open(cryptFilename, FileMode.Open);
+                FileStream writer = File.Open(storageName, FileMode.Create);
+
+                try
+                {
+                    getPassword();
+                    byte[] temp = new byte[16];
+                    int whole = (int)(reader.Length) / 16;
+                    int residue = (int)(reader.Length) % 16;
+                    progressAdd.Maximum = whole;
+                    for (int i = 0; i < whole; i++)
+                    {
+                        reader.Read(temp, 0, 16);
+                        temp = Decrypt(temp, password16, IV);
+                        writer.Write(temp, 0, 16);
+                        progressAdd.Value++;
+                    }
+                    if (residue != 0)
+                    {
+                        reader.Read(temp, 0, residue);
+                        temp = Decrypt(temp, password16, IV);
+                        writer.Write(temp, 0, residue);
+                    }
+                    progressAdd.Value = 0;
+                    reader.Close();
+                    writer.Close();
+                    MessageBox.Show("Расшифровка завершена!");
+                    storagePath = storageName;
+                    labelStoragePath.Text = storagePath;
+                    createFilesList(storagePath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    reader.Close();
+                    writer.Close();
+                }
+            }
+        }
+
+        private string getReadableFileSize(long size)
+        {
+            return "";
+        }
+    }
+}
